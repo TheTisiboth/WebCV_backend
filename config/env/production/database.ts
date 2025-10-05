5d9afd433dd4d5bce5deb2f11c8a72191ba87864
Léo Jan <leo.jan@doctolib.com>
2025-10-05 19:29:48 +0200

    Disable ssl for postgres

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -12,9 +12,7 @@ export default ({ env }) => ({
             database: config.database,
             user: config.user,
             password: config.password,
-            ssl: {
-                rejectUnauthorized: false
-            }
+            ssl: false
         },
     },
     debug: false,
